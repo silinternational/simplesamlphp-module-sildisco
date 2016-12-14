923b7ae753b1813cf7fd9dafae173ef363a5665f
Baggerone <baggerone@gmail.com>
2016-12-14 11:45:28 -0500

    Removed unneeded lines and added tests

--- tests/AuthProcTest.php
+++ tests/AuthProcTest.php
@@ -1,16 +1,23 @@
 <?php
-//namespace Sil\SilDiscoTests;
 
-//include __DIR__ . '../../../../vendor/autoload.php';
-
-//use PHPUnit\Framework\TestCase;
-//use Sil\SspUtils\Utils;
-//use Sil\SspUtils\Metadata;
-
-//use sspmod_sildisco_Auth_Process_AddIdp2NameId;
 
 class AuthProcTest extends PHPUnit_Framework_TestCase
 {
+
+    private static function getNameID($idp) {
+        return [
+            'saml:sp:IdP' => $idp,
+            'saml:sp:NameID' => [
+                [
+                    'Format' => 'urn:oasis:names:tc:SAML:1.1:nameid-format:unspecified',
+                    'Value' => 'Tester1_Smith',
+                    'SPNameQualifier' => 'http://ssp-hub-sp.local',
+                ],
+            ],
+            'Attributes' => [],
+            'metadataPath' => __DIR__ . '/fixtures/metadata/',
+        ];
+    }
 
     /**
      * Helper function to run the filter with a given configuration.
@@ -19,7 +26,7 @@ class AuthProcTest extends PHPUnit_Framework_TestCase
      * @param array $request  The request state.
      * @return array  The state array after processing.
      */
-    private static function processFilter(array $config, array $request)
+    private static function processAddIdp2NameId(array $config, array $request)
     {
         $filter = new sspmod_sildisco_Auth_Process_AddIdp2NameId($config, NULL);
         $filter->process($request);
@@ -27,21 +34,73 @@ class AuthProcTest extends PHPUnit_Framework_TestCase
     }
 
     /*
-     *
-     *
+     * Test with IdP metadata not having an IDPCode entry
+     * @expectedException SimpleSAML_Error_Exception
      */
-    public function testAddIdp2NameId_NoNameId()
+    public function testAddIdp2NameId_NoIDPCode()
     {
-        $config = array(
-            'test' => array('value1', 'value2'),
-        );
-        $request = array(
-            'Attributes' => array(),
-        );
+        $this->setExpectedException('SimpleSAML_Error_Exception');
+        $config = [ 'test' => ['value1', 'value2'], ];
+        $request = self::getNameID('idp-bare');
 
-        $expected = ['stub_expected'];
+        self::processAddIdp2NameId($config, $request);
+    }
 
-        $results = self::processFilter($config, $request);
+
+    /*
+     * Test with IdP metadata not having an IDPCode entry
+     * @expectedException SimpleSAML_Error_Exception
+     */
+    public function testAddIdp2NameId_EmptyIDPCode()
+    {
+        $this->setExpectedException('SimpleSAML_Error_Exception');
+        $config = [ 'test' => ['value1', 'value2'], ];
+        $request = self::getNameID('idp-empty');
+        self::processAddIdp2NameId($config, $request);
+    }
+
+    /*
+     * Test with IdP metadata not having an IDPCode entry
+     * @expectedException SimpleSAML_Error_Exception
+     */
+    public function testAddIdp2NameId_BadIDPCode()
+    {
+        $this->setExpectedException('SimpleSAML_Error_Exception');
+        $config = [
+            'test' => ['value1', 'value2'],
+        ];
+        $request = self::getNameID('idp-bad');
+        self::processAddIdp2NameId($config, $request);
+    }
+
+    /*
+     * Test with IdP metadata having a good IDPCode entry
+     */
+    public function testAddIdp2NameId_Good()
+    {
+        $config = ['test' => ['value1', 'value2']];
+        $request = [
+            'saml:sp:IdP' => 'idp-good',
+            'saml:sp:NameID' => [
+                [
+                    'Format' => 'urn:oasis:names:tc:SAML:1.1:nameid-format:unspecified',
+                    'Value' => 'Tester1_SmithA',
+                    'SPNameQualifier' => 'http://ssp-hub-sp.local',
+                ],
+                [
+                    'Format' => 'urn:oasis:names:tc:SAML:1.1:nameid-format:transient',
+                    'Value' => 'Tester1_SmithB',
+                    'SPNameQualifier' => 'http://ssp-hub-sp.local',
+                ],
+            ],
+            'Attributes' => [],
+            'metadataPath' => __DIR__ . '/fixtures/metadata/',
+        ];
+
+        $results = self::processAddIdp2NameId($config, $request);
+        $expected = $results;
+        $expected['saml:sp:NameID'][0]['Value'] = 'Tester1_SmithA@idp-good';
+        $expected['saml:sp:NameID'][1]['Value'] = 'Tester1_SmithB@idp-good';
 
         $this->assertEquals($expected, $results);
     }
