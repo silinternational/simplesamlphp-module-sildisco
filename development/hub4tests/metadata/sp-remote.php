3ee3648a6805609f4a52a722b7668fae9a7458b1
Baggerone <baggerone@gmail.com>
2017-04-13 13:13:57 -0400

    changed urls for automated testing within docker

--- development/hub4tests/metadata/sp-remote.php
+++ development/hub4tests/metadata/sp-remote.php
@@ -10,19 +10,19 @@ return [
      * Example SimpleSAMLphp SAML 2.0 SP
      */
     'http://ssp-hub-sp.local' => [
-        'AssertionConsumerService' => 'http://ssp-hub-sp.local:8081/module.php/saml/sp/saml2-acs.php/hub4tests',
-        'SingleLogoutService' => 'http://ssp-hub-sp.local:8081/module.php/saml/sp/saml2-logout.php/hub4tests',
+        'AssertionConsumerService' => 'http://sp1/module.php/saml/sp/saml2-acs.php/hub4tests',
+        'SingleLogoutService' => 'http://sp1/module.php/saml/sp/saml2-logout.php/hub4tests',
     ],
 
     'http://ssp-hub-sp2.local' => [
-        'AssertionConsumerService' => 'http://ssp-hub-sp2.local:8082/module.php/saml/sp/saml2-acs.php/hub4tests',
-        'SingleLogoutService' => 'http://ssp-hub-sp2.local:8082/module.php/saml/sp/saml2-logout.php/hub4tests',
+        'AssertionConsumerService' => 'http://sp2/module.php/saml/sp/saml2-acs.php/hub4tests',
+        'SingleLogoutService' => 'http://sp2/module.php/saml/sp/saml2-logout.php/hub4tests',
         'IDPList' => ['http://ssp-hub-idp2.local:8086'],
     ],
 // This one should be on the SPList entry of idp2
     'http://ssp-hub-sp3.local' => [
-        'AssertionConsumerService' => 'http://ssp-hub-sp3.local:8083/module.php/saml/sp/saml2-acs.php/hub4tests',
-        'SingleLogoutService' => 'http://ssp-hub-sp3.local:8083/module.php/saml/sp/saml2-logout.php/hub4tests',
+        'AssertionConsumerService' => 'http://sp3/module.php/saml/sp/saml2-acs.php/hub4tests',
+        'SingleLogoutService' => 'http://sp3/module.php/saml/sp/saml2-logout.php/hub4tests',
     ],
 
 ];
