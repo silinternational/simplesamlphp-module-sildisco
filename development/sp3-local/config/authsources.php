3a4618410f19fa8526668651a6107d154786bb96
Baggerone <baggerone@gmail.com>
2017-04-13 13:12:49 -0400

    added stanza for hub4tests

--- development/sp3-local/config/authsources.php
+++ development/sp3-local/config/authsources.php
@@ -29,5 +29,21 @@ $config = array(
         'discoURL' => null,
     ),
 
+    'hub4tests' => array(
+        'saml:SP',
+
+        // The entity ID of this SP.
+        // Can be NULL/unset, in which case an entity ID is generated based on the metadata URL.
+        'entityID' => 'http://ssp-hub-sp3.local',
+
+        // The entity ID of the IdP this should SP should contact.
+        // Can be NULL/unset, in which case the user will be shown a list of available IdPs.
+        'idp' => 'hub4tests',
+
+        // The URL to the discovery service.
+        // Can be NULL/unset, in which case a builtin discovery service will be used.
+        'discoURL' => null,
+    ),
+
 
 );
