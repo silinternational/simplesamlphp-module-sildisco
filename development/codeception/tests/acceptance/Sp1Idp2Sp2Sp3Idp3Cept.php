0e9b36e78ac42898f650cc5fb9a14c76e9480c90
Baggerone <baggerone@gmail.com>
2017-04-17 11:26:23 -0400

    Initial commit

---
+++ development/codeception/tests/acceptance/Sp1Idp2Sp2Sp3Idp3Cept.php
@@ -0,0 +1,54 @@
+<?php 
+
+$waitTime = 10;
+
+$I = new AcceptanceTester($scenario);
+$I->wantTo('Ensure I can login to Sp1 through Idp2, am already logged in for Sp2, and must login to Sp3 through Idp1.');
+
+// Start at sp1
+$I->amOnUrl('http://sp1');
+$I->waitForText('About SimpleSAMLphp', $waitTime);
+
+$I->click('Authentication');
+$I->click('Test configured authentication sources');
+$I->waitForText('Test authentication sources', $waitTime);
+
+// Go to the hub
+$I->click('hub4tests');
+$I->waitForText('IdP 2', $waitTime);
+
+$I->seeCurrentUrlMatches("~/module.php/sildisco/disco.php\?entityID=hub4tests~");
+
+// Use idp1 for Authentication
+$I->click(["name" => "idp_http://ssp-hub-idp2.local:8086"]);
+$I->waitForText("Enter your username and password", $waitTime);
+
+$I->fillField('password', 'b');
+$I->click('//*[@id="regularsubmit"]/td[3]/button');
+
+$I->waitForText("@IDP2", $waitTime);
+
+
+// Start at sp2. Go through hub to idp2
+$I->amOnUrl('http://sp2/module.php/core/authenticate.php?as=hub4tests');
+$I->waitForText("@IDP2", $waitTime);
+
+// See that going to sp3 results in immediate authentication to idp1
+$I->amOnUrl('http://sp3/module.php/core/authenticate.php?as=hub4tests');
+$I->waitForText("Enter your username and password", $waitTime);
+
+$I->fillField('password', 'a');
+$I->click('//*[@id="regularsubmit"]/td[3]/button');
+
+
+$I->waitForText("test_admin@idp1.org", $waitTime);
+
+// Logout of both IDP's
+$I->click("Logout");
+$I->waitForText("You have been logged out.", $waitTime);
+
+
+$I->amOnUrl('http://sp2/module.php/core/authenticate.php?as=hub4tests');
+$I->waitForText("@IDP2", $waitTime);
+$I->click("Logout");
+$I->waitForText("You have been logged out.", $waitTime);
---
+++ development/codeception/tests/acceptance/Sp2Idp2Sp1Idp2Sp3Cept.php
@@ -0,0 +1,51 @@
+<?php 
+
+$waitTime = 10;
+
+$I = new AcceptanceTester($scenario);
+$I->wantTo('Ensure I can login to Sp2 through Idp2, get discovery page for Sp1, and must login to Sp3 through Idp1.');
+
+// Start at sp2. Go through hub to idp2
+$I->amOnUrl('http://sp2/module.php/core/authenticate.php?as=hub4tests');
+$I->waitForText("Enter your username and password", $waitTime);
+
+$I->fillField('password', 'b');
+$I->click('//*[@id="regularsubmit"]/td[3]/button');
+
+$I->waitForText("@IDP2", $waitTime);
+
+// Start at sp1
+$I->amOnUrl('http://sp1/module.php/core/authenticate.php?as=hub4tests');
+$I->wait(5);
+$I->makeScreenshot('debug');
+
+// Wait for redirect to the hub
+$I->waitForText('IdP 1', $waitTime);
+
+$I->seeCurrentUrlMatches("~/module.php/sildisco/disco.php\?entityID=hub4tests~");
+
+// Use idp2 but already Authenticated
+$I->click(["name" => "idp_http://ssp-hub-idp2.local:8086"]);
+
+$I->waitForText("@IDP2", $waitTime);
+
+
+// See that going to sp3 requires login
+$I->amOnUrl('http://sp3/module.php/core/authenticate.php?as=hub4tests');
+
+$I->waitForText("Enter your username and password", $waitTime);
+
+$I->fillField('password', 'a');
+$I->click('//*[@id="regularsubmit"]/td[3]/button');
+
+$I->waitForText("test_admin@idp1.org", $waitTime);
+
+// Logout of both idp's
+$I->click('Logout');
+$I->waitForText("You have been logged out.", $waitTime);
+
+$I->amOnUrl('http://sp2/module.php/core/authenticate.php?as=hub4tests');
+$I->waitForText("@IDP2", $waitTime);
+$I->click("Logout");
+$I->waitForText("You have been logged out.", $waitTime);
+
