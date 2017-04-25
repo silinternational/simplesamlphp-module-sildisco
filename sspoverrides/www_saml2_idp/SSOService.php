4631ebb97287eb6ed234dc84539486b25766633f
Baggerone <baggerone@gmail.com>
2017-04-25 10:24:11 -0400

    Use HUB_MODE env to decide where the entry script should direct the request

--- sspoverrides/www_saml2_idp/SSOService.php
+++ sspoverrides/www_saml2_idp/SSOService.php
@@ -22,9 +22,25 @@ $metadata = SimpleSAML_Metadata_MetaDataStorageHandler::getMetadataHandler();
 $idpEntityId = $metadata->getMetaDataCurrentEntityID('saml20-idp-hosted');
 $idp = SimpleSAML_IdP::getById('saml2:' . $idpEntityId);
 
+$config = SimpleSAML_Configuration::getConfig();
+$hubModeKey = 'hubmode';
 
+// If in hub mode, then use the sildisco entry script
+if ($config->hasValue($hubModeKey) and $config->getValue($hubModeKey)) {
+    try {
+        sspmod_sildisco_IdP_SAML2::receiveAuthnRequest($idp);
+    } catch (Exception $e) {
+        if ($e->getMessage() === "Unable to find the current binding.") {
+            throw new SimpleSAML_Error_Error('SSOPARAMS', $e, 400);
+        } else {
+            throw $e; // do not ignore other exceptions!
+        }
+    }
+}
+
+// If not in hub mode, use the out-of-the-box simplesamlphp entry script
 try {
-    sspmod_sildisco_IdP_SAML2::receiveAuthnRequest($idp);
+    sspmod_saml_IdP_SAML2::receiveAuthnRequest($idp);
 } catch (Exception $e) {
     if ($e->getMessage() === "Unable to find the current binding.") {
         throw new SimpleSAML_Error_Error('SSOPARAMS', $e, 400);
