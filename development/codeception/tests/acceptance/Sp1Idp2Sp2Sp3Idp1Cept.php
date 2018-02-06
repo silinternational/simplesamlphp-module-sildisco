b6ee39b3f4911820107d429e1bd933da960d8d79
Unknown <baggerone@gmail.com>
2018-02-06 10:31:37 -0500

    Changed name from having Idp3 near the end. Looks for correct text now.

--- development/codeception/tests/acceptance/Sp1Idp2Sp2Sp3Idp1Cept.php
+++ development/codeception/tests/acceptance/Sp1Idp2Sp2Sp3Idp1Cept.php
@@ -2,12 +2,13 @@
 
 $waitTime = 10;
 $idp2Id =  '//*[@id="http://ssp-hub-idp2.local:8086"]';
+$spHomePath = '/module.php/core/frontpage_welcome.php';
 
 $I = new AcceptanceTester($scenario);
 $I->wantTo('Ensure I can login to Sp1 through Idp2, am already logged in for Sp2, and must login to Sp3 through Idp1.');
 
 // Start at sp1
-$I->amOnUrl('http://sp1');
+$I->amOnUrl('http://sp1' . $spHomePath);
 $I->waitForText('About SimpleSAMLphp', $waitTime);
 
 $I->click('Authentication');
@@ -27,14 +28,14 @@ $I->waitForText("Enter your username and password", $waitTime);
 $I->fillField('password', 'b');
 $I->click('//*[@id="submit"]/td[3]/button');
 
-$I->waitForText("@IDP2", $waitTime);
+$I->waitForText("http://ssp-hub-sp.local", $waitTime);
 
 
 // Start at sp2. Go through hub to idp2
 $I->amOnUrl('http://sp2/module.php/core/authenticate.php?as=hub4tests');
-$I->waitForText("@IDP2", $waitTime);
+$I->waitForText("http://ssp-hub-sp2.local", $waitTime);
 
-// See that going to sp3 results in immediate authentication to idp1
+// See that going to sp3 results bypassing the hub and going straight to idp1
 $I->amOnUrl('http://sp3/module.php/core/authenticate.php?as=hub4tests');
 $I->waitForText("Enter your username and password", $waitTime);
 
@@ -50,6 +51,6 @@ $I->waitForText("You have been logged out.", $waitTime);
 
 
 $I->amOnUrl('http://sp2/module.php/core/authenticate.php?as=hub4tests');
-$I->waitForText("@IDP2", $waitTime);
+$I->waitForText("http://ssp-hub-sp2.local", $waitTime);
 $I->click("Logout");
 $I->waitForText("You have been logged out.", $waitTime);
