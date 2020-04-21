544fa73d83b9bae00c9d5f3753f771babe22baaa
Schparky <3172830+Schparky@users.noreply.github.com>
2020-04-21 08:13:26 -0600

    change nameid format to persistent

--- lib/Auth/Process/AddIdp2NameId.php
+++ lib/Auth/Process/AddIdp2NameId.php
@@ -160,7 +160,7 @@ class AddIdp2NameId extends \SimpleSAML\Auth\ProcessingFilter {
         $nameId = $state[self::SP_NAMEID_ATTR];
         self::appendIdp($nameId, $IDPNamespace);
 
-        $format =  'urn:oasis:names:tc:SAML:1.1:nameid-format:unspecified';
+        $format =  'urn:oasis:names:tc:SAML:2.0:nameid-format:persistent';
 
         if ( ! empty($this->format)) {
             $format = $this->format;
