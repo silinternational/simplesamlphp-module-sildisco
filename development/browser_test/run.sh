6d6d4ef0a0997fe450a1d5fdd5e0ae671342f7d5
Steve Bagwell <steve_bagwell@sil.org>
2022-08-23 14:48:11 -0400

    revert troubleshooting attempt

--- development/browser_test/run.sh
+++ development/browser_test/run.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 
-echo "running \"codecept build\" from run.sh"
-
-/data/codecept build
-# If that failed, exit.
-rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+echo "Empty run.sh"
 
 # Run apache in foreground
 apache2ctl -D FOREGROUND
